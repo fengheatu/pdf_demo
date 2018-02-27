532c1c9b1e1f212b4b19ceb90964ebe3d1a78d39
fenghe <fengheatu@163.com>
2018-02-27 14:39:07 +0800

    创建pdf

--- src/main/java/com/river/pdf/PDFTest.java
+++ src/main/java/com/river/pdf/PDFTest.java
@@ -2,15 +2,20 @@ package com.river.pdf;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.itextpdf.text.Document;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.*;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
+import java.awt.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import static com.itextpdf.text.Font.BOLDITALIC;
 
 /**
  * @author: he.feng
@@ -65,6 +70,38 @@ public class PDFTest {
         }
     }
 
+    @Test
+    public void createPDF() throws Exception{
+        Document document = new Document(PageSize.A4);
+        OutputStream outputStream = new FileOutputStream("d:/888.pdf");
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, outputStream);
+        BaseFont baseFont =  BaseFont.createFont("STSong-Light", "UniGB-UCS2-H",BaseFont.NOT_EMBEDDED);
+        Font chinese = new Font(baseFont, 20, Font.BOLDITALIC);
+        document.open();
+        document.add(new Paragraph("如果要设定第一页的页面属性，这些方法必须在文档打开之前调用。\n" +
+                "　　对于PDF文档，iText还提供了文档的显示属性，通过调用书写器的setViewerPreferences方法可以控制文档打开时Acrobat Reader的显示属性，如是否单页显示、是否全屏显示、是否隐藏状态条等属性。\n" +
+                "　　另外，iText也提供了对PDF文件的安全保护，通过书写器(Writer)的setEncryption方法，可以设定文档的用户口令、只读、可打印等属性。\n" +
+                "　　添加文档内容\n" +
+                "　　所有向文档添加的内容都是以对象为单位的，如Phrase、Paragraph、Table、Graphic对象等。比较常用的是段落(Paragraph)对象，用于向文档中添加一段文字。\n" +
+                "　　三、文本处理\n" +
+                "　　iText中用文本块(Chunk)、短语(Phrase)和段落(paragraph)处理文本。\n" +
+                "　　文本块(Chunk)是处理文本的最小单位，有一串带格式(包括字体、颜色、大小)的字符串组成。如以下代码就是产生一个字体为HELVETICA、大小为10、带下划线的字符串：\n" +
+                "　　Chunk chunk1 = new Chunk(\"This text is underlined\", FontFactory.getFont(FontFactory.HELVETICA, 12, Font.UNDERLINE));\n" +
+                "　　短语(Phrase)由一个或多个文本块(Chunk)组成，短语(Phrase)也可以设定字体，但对于其中以设定过字体的文本块 (Chunk)无效。通过短语(Phrase)成员函数add可以将一个文本块(Chunk)加到短语(Phrase)中，如：phrase6.add(chunk);\n" +
+                "　　段落(paragraph)由一个或多个文本块(Chunk)或短语(Phrase)组成，相当于WORD文档中的段落概念，同样可以设定段落的字体大小、颜色等属性。另外也可以设定段落的首行缩进、对齐方式(左对齐、右对齐、居中对齐)。通过函数setAlignment可以设定段落的对齐方式， setAlignment的参数1为居中对齐、2为右对齐、3为左对齐，默认为左对齐。\n" +
+                "　　四、表格处理\n" +
+                "　　iText中处理表格的类为：com.lowagie.text.Table和com.lowagie.text.PDF.PDFPTable，对于比较简单的表格处理可以用com.lowagie.text.Table，但是如果要处理复杂的表格，这就需要 com.lowagie.text.PDF.PDFPTable进行处理。这里就类com.lowagie.text.Table进行说明。\n" +
+                "　　类com.lowagie.text.Table的构造函数有三个：\n" +
+                "　　①Table (int columns)\n" +
+                "　　②Table(int columns, int rows)\n" +
+                "　　③Table(Properties attributes)\n" +
+                "　　参数columns、rows、attributes分别为表格的列数、行数、表格属性。创建表格时必须指定表格的列数，而对于行数可以不用指定。\n" +
+                "　　建立表格之后，可以设定表格的属性，如：边框宽度、边框颜色、衬距(padding space 即单元格之间的间距)大小等属性。下面通过一个简单的例子说明如何使用表格，代码如下：",chinese));
+        document.close();
+        pdfWriter.close();
+        outputStream.close();
+
+    }
 
 
 }
---
+++ src/main/java/com/river/pdf/PDFUtil.java
@@ -0,0 +1,18 @@
+package com.river.pdf;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+/**
+ * @author: he.feng
+ * @date: 14:30 2018/2/27
+ * @desc:
+ **/
+public class PDFUtil {
+
+
+}
